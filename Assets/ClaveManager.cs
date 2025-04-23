2c691de2eec63cddcffe8775f90bda42fc252b12
mercho40 <107892036+mercho40@users.noreply.github.com>
2025-04-23 15:39:23 -0300

    anda

---
+++ Assets/ClaveManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ClaveManager : MonoBehaviour
+{
+    public TMP_InputField inpuClave;
+    public TextMeshProUGUI txtMensaje;
+    string clave = "tic2025";
+    void Start()
+    {
+        txtMensaje.text = string.Empty;
+    }
+
+    public void ComprobarClaveIngresada()
+    {
+        string claveIngresada = inpuClave.text;
+        if (claveIngresada != clave)
+        {
+            txtMensaje.text = "Acceso denegado";
+            inpuClave.text = "";
+        }
+        else
+        {
+            txtMensaje.text = "Bienvenido";
+        }
+    }
+}
